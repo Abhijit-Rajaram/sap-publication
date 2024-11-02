98e5d1e3b8a17bd4a5115871659bf899eb393e21
Abhijit-Rajaram <abhijitrajaram.developer@gmail.com>
2024-11-03 00:54:19 +0530

    Leading slash in static_url

--- book/settings.py
+++ book/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
